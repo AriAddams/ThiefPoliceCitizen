4201ab29c9f4fc388dcfb2fd09052f0319befeb3
arina <arina@ARINA_ADDAMS>
2024-10-21 19:22:21 +0200

    Add project files.

---
+++ ThiefPoliceCitizen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThiefPoliceCitizen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
